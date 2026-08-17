973b3c5c1a59df985e6ec25036f261f091976d64
Yakov <your@email.com>
2026-08-17 03:18:44 +0300

    Добавлена почта blyashka1@gmail.com в контакты

--- app/components/ContactModal.tsx
+++ app/components/ContactModal.tsx
@@ -51,6 +51,16 @@ export default function ContactModal({ isOpen, onClose }) {
             </div>
             <span className="contact-name">Instagram</span>
           </a>
+
+          <a href="mailto:blyashka1@gmail.com" className="contact-item">
+            <div className="contact-icon">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#c4b5a0" className="icon-svg">
+                <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"/>
+                <polyline points="22,6 12,13 2,6"/>
+              </svg>
+            </div>
+            <span className="contact-name">Email</span>
+          </a>
         </div>
 
         <style jsx>{`
